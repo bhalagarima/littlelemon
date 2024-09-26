b05150c48fbbd433f323fc47c011e834f12221dc
Garima Bhala <garimabhala@macbookair.home>
2024-09-26 16:08:17 -0400

    core data added

---
+++ Little Lemon/Little Lemon/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Little Lemon
+//
+//  Created by Garima Bhala on 2024-09-26.
+//
+
+import Foundation
---
+++ Little Lemon/Little Lemon/MenuList.swift
@@ -0,0 +1,8 @@
+//
+//  MenuList.swift
+//  Little Lemon
+//
+//  Created by Garima Bhala on 2024-09-26.
+//
+
+import Foundation
